9eaf2c20790c25fe3ffb5361893250fd97e8332a
Yamada Masaki <masaki@hisme.net>
2011-04-10 03:14:13 +0900

    nameは必須の設定

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Character do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @valid_attributes = {
+      :name => 'hoge'
+    }
+  end
+  
+  describe "#valid?" do
+    subject { Character.new(@valid_attributes) }
+    it { should be_valid }
+    describe "nameは必須" do
+      subject { Character.new }
+      it { should_not be_valid }
+    end
+  end
 end
