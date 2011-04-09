c7e04984096b88ea9551338340c625a80d6d66a3
Yamada Masaki <masaki@hisme.net>
2011-04-10 01:09:30 +0900

    titleは必須の設定追加

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,4 +1,22 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Story do
+  describe Story do
+    before do
+      @valid_attributes = {
+        :title => 'ほげ'
+      }
+    end
+    
+    describe "#valid?" do
+      subject { Story.new(@valid_attributes) }
+      it { should be_valid }
+      its(:save) { should be_true }
+      describe "titleは必須" do
+        subject { Story.new }
+        it { should_not be_valid }
+      end
+    end
+  end
 end
