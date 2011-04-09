087bfeaa8ddd6c4bfd96fc1a4daa74e17f01f1db
Yamada Masaki <masaki@hisme.net>
2011-04-09 23:07:48 +0900

    じぇねれーと

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'parent_resource'
+gem 'rack-openid'
+gem 'ruby-openid-apps-discovery'
+gem 'dynamic_form'
+gem 'gravatar_image_tag'
+
+group :test, :development do
+  gem "rails_code_qa"
+  gem "rspec-rails"
+  gem "auto_mock"
+  gem "webrat"
+  gem "autotest"
+  gem "rcov"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SsEditor::Application.config.secret_token = '84157381ced2180a5879801d352b9e46fa35cd02d2d9e0dbc5a5a01abc780107ca3739b392f77b04723181a813e87897b4a424735001be55803b353ea3e7aeaf'
