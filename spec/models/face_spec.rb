0552f6b05b13564cbb9cc22b0cc7b7bdad709951
Yamada Masaki <masaki@hisme.net>
2011-04-16 04:48:22 +0900

    faceモデルのvalidationとassociation設定

--- spec/models/face_spec.rb
+++ spec/models/face_spec.rb
@@ -1,5 +1,25 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Face do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :faces, :characters
+  before do
+    @valid_attributes = {
+      :name => 'アヘ顔',
+      :character => characters(:mio_akiyama)
+    }
+  end
+  
+  describe '#valid?' do
+    subject { Face.new(@valid_attributes) }
+    it { should be_valid }
+    describe "name は必須" do
+      subject { Face.new(:character => characters(:mio_akiyama)) }
+      it { should_not be_valid }
+    end
+    describe "characeter_id は必須" do
+      subject { Face.new(:name => 'アヘ顔') }
+      it { should_not be_valid }
+    end
+  end
 end
