1202d3b9e079e4b15cfb222e631f67876ae5129a
Yamada Masaki <masaki@hisme.net>
2011-04-16 05:12:33 +0900

    facesのroutesのspecを変更

--- spec/routing/faces_routing_spec.rb
+++ spec/routing/faces_routing_spec.rb
@@ -4,31 +4,31 @@ describe FacesController do
   describe "routing" do
 
     it "recognizes and generates #index" do
-      { :get => "/faces" }.should route_to(:controller => "faces", :action => "index")
+      { :get => "/characters/1/faces" }.should route_to(:controller => "faces", :action => "index", :character_id => "1")
     end
 
     it "recognizes and generates #new" do
-      { :get => "/faces/new" }.should route_to(:controller => "faces", :action => "new")
+      { :get => "/characters/1/faces/new" }.should route_to(:controller => "faces", :action => "new", :character_id => "1")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/faces/1" }.should route_to(:controller => "faces", :action => "show", :id => "1")
+      { :get => "/characters/1/faces/1" }.should route_to(:controller => "faces", :action => "show", :id => "1", :character_id => "1")
     end
 
     it "recognizes and generates #edit" do
-      { :get => "/faces/1/edit" }.should route_to(:controller => "faces", :action => "edit", :id => "1")
+      { :get => "/characters/1/faces/1/edit" }.should route_to(:controller => "faces", :action => "edit", :id => "1", :character_id => "1")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/faces" }.should route_to(:controller => "faces", :action => "create")
+      { :post => "/characters/1/faces" }.should route_to(:controller => "faces", :action => "create", :character_id => "1")
     end
 
     it "recognizes and generates #update" do
-      { :put => "/faces/1" }.should route_to(:controller => "faces", :action => "update", :id => "1")
+      { :put => "/characters/1/faces/1" }.should route_to(:controller => "faces", :action => "update", :id => "1", :character_id => "1")
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/faces/1" }.should route_to(:controller => "faces", :action => "destroy", :id => "1")
+      { :delete => "/characters/1/faces/1" }.should route_to(:controller => "faces", :action => "destroy", :id => "1", :character_id => "1")
     end
 
   end
