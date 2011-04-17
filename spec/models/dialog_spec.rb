72b3ec7ae2af0135a2249eede3d3d3744a232ea3
Yamada Masaki <masaki@hisme.net>
2011-04-18 05:53:59 +0900

    変化を見るようにspecを変更 story_idが指定されてない場合のspecを追加

--- spec/models/dialog_spec.rb
+++ spec/models/dialog_spec.rb
@@ -28,14 +28,24 @@ describe Dialog do
   
   describe "#set_default_line_num" do
     it "同じStoryのDialogうち一番大きいline_num + 1が line_num に設定される" do
-      dialog = Dialog.new(:story => stories(:ritsu_x_mio))
-      dialog.send(:set_default_line_num)
-      dialog.line_num.should be(2)
+      dialog = Dialog.new(:story => stories(:ritsu_x_mio), :line_num => 10)
+      lambda{
+        dialog.send(:set_default_line_num)
+      }.should change(dialog, :line_num).from(10).to(2)
     end
+    
     it "同じStoryのDialogが存在しない場合 line_num が1に設定される" do
-      dialog = Dialog.new(:story => stories(:yui_x_azu))
-      dialog.send(:set_default_line_num)
-      dialog.line_num.should be(1)
+      dialog = Dialog.new(:story => stories(:yui_x_azu), :line_num => 10)
+      lambda{
+        dialog.send(:set_default_line_num)
+      }.should change(dialog, :line_num).from(10).to(1)
+    end
+    
+    it "story_idが指定されてない場合は何もしない" do
+      dialog = Dialog.new
+      lambda{
+        dialog.send(:set_default_line_num)
+      }.should_not change(dialog, :line_num)
     end
   end
 end
