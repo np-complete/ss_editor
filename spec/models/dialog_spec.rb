a2e7933b20523a3c53842143fb5149d0918248aa
Yamada Masaki <masaki@hisme.net>
2011-04-18 04:44:46 +0900

    Dialogのspec追加

--- spec/models/dialog_spec.rb
+++ spec/models/dialog_spec.rb
@@ -1,5 +1,24 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Dialog do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :dialogs, :characters, :faces, :stroies
+  before do
+    @valid_attributes = {
+      :character => characters(:ritsu_tainaka),
+      :face => faces(:ritsu_ahegao),
+      :story => stories(:ritsu_x_mio),
+      :line_num => 2,
+      :message => 'ほげほげほげほげ'
+    }
+  end
+  
+  describe "#valid?" do
+    subject { Dialog.new(@valid_attributes) }
+    it { should be_valid }
+    describe "[story, line_num]はunique" do
+      subject { Dialog.new(@valid_attributes.merge(:line_num => 1)) }
+      it { should_not be_valid }
+    end
+  end
 end
