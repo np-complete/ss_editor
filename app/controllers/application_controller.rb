ded25ef9979469877f236f6efc420926d2e8a25c
Yamada Masaki <masaki@hisme.net>
2011-05-08 07:22:38 +0900

    ApplicationControllerにauth関連のメソッド実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :auth
+  
+  private
+  def auth
+    if session[:identity_url]
+      @auth = User.auth(session[:identity_url])
+    end
+  end
+  
+  def login?
+    @auth.present?
+  end
 end
