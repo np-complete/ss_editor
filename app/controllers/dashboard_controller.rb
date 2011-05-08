d3e01e37d69c0055431da85b643e075d2c1d9bae
Yamada Masaki <masaki@hisme.net>
2011-05-08 23:42:27 +0900

    ログインの処理追加

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,14 @@ class DashboardController < ApplicationController
   end
   
   def login
+    authenticate_with_open_id("https://www.google.com/accounts/o8/id") do |result, identity_url, registration|
+      if result.successful?
+        session[:identity_url] = identity_url
+        redirect_to :controller => :dashboard
+      else
+        render :text => "Login failed: #{result.message}"
+      end
+    end
   end
   
   def logout
