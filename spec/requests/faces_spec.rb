339985ed48b6fb1ffec089fc7fcafed9887f3fe1
Yamada Masaki <masaki@hisme.net>
2011-05-14 03:57:15 +0900

    face.urlを呼ぶことにする

--- spec/requests/faces_spec.rb
+++ spec/requests/faces_spec.rb
@@ -7,7 +7,9 @@ describe "Faces" do
     end
     
     it "works! (now write some real specs)" do
-      mock_character.stub(:faces) { [mock_face(:name => 'hoge', :icon => 'unko')] }
+      mock_character.stub(:faces) { [
+          mock_face(:name => 'hoge', :url => 'unko')
+        ] }
       get character_faces_path(mock_character)
       response.status.should be(200)
     end
