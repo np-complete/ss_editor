04244fba48b1c9077167115645156cd604331183
Yamada Masaki <masaki@hisme.net>
2011-05-29 04:11:34 +0900

    テーブル定義変更

--- db/migrate/20110415193025_create_faces.rb
+++ db/migrate/20110415193025_create_faces.rb
@@ -3,9 +3,11 @@ class CreateFaces < ActiveRecord::Migration
     create_table :faces do |t|
       t.string :name, :null => false
       t.integer :character_id, :null => false
+      t.integer :user_id, :null => false
       t.timestamps
     end
-    add_index :faces, :character_id
+    add_index :faces, [:character_id, :user_id]
+    add_index :faces, :user_id
   end
 
   def self.down
