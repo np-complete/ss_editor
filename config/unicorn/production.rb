add57250999de3b23c4cc0579cf15d7c013d6448
masarakki <masaki@hisme.net>
2013-03-30 06:55:10 +0900

    fix unicorn conf

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,12 +1,13 @@
-working_directory '/home/masaki/www/ss_editor/current'
+RAILS_ROOT = '/home/masaki/www/ss_editor/current'
+working_directory RAILS_ROOT
 
-listen File.join(working_directory, 'tmp/pids/.sock')
+listen File.expand_path('tmp/pids/.sock', RAILS_ROOT)
 
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stderr_path File.expand_path('log/unicorn.log', RAILS_ROOT)
+stdout_path File.expand_path('log/unicorn.log', RAILS_ROOT)
 
 preload_app true
-pid File.expand_path('tmp/pids/unicorn.pid', ENV['RAILS_ROOT'])
+pid File.expand_path('tmp/pids/unicorn.pid', RAILS_ROOT)
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
